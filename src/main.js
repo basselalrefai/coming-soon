8d3b3bda0f7f72d71a0f1d0c7c8159a135fd44fa
Bassel Alrefai <bassel@basselalrefai.com>
2020-05-13 13:58:50 -0400

    Link javascript and set global variables

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// Global Variables
+
+const button = document.getElementsByClassName("form-button-main");
+const input = document.getElementsByClassName("form-email");
+const errorIcon = document.getElementsByClassName("form-error-icon");
+const errorMessage = document.getElementsByClassName("error-message");
+
+console.log(button);
+console.log('hello')
