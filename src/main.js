54e7bc4b84a3e2ecdef1cd68633c89120c01f078
Bassel Alrefai <bassel@basselalrefai.com>
2020-05-13 14:42:09 -0400

    Hide error message when click outside of form

--- src/main.js
+++ src/main.js
@@ -9,8 +9,20 @@ console.log(errorIcon)
 // show error message
 button.addEventListener("click", function() {
     if (input.validity.typeMismatch) {
+        errorMessage.textContent="Please provide a valid email";
         errorMessage.style.visibility = "visible";
         errorIcon.style.visibility = "visible";
         input.setCustomValidity(' ');
+    } else {
+        errorMessage.textContent="Success";
+        errorMessage.style.visibility = "visible";
+        errorIcon.style.visibility = "hidden"; 
+        input.setCustomValidity(' ');  
     }
-})
+});
+
+// hide error message when focus is away
+input.onblur = function() {
+    errorMessage.style.visibility = "hidden";
+    errorIcon.style.visibility = "hidden";
+}
