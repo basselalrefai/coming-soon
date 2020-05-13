6320696b3cc65cf7e527fb11b66e9ea2af0e0f2d
Bassel Alrefai <bassel@basselalrefai.com>
2020-05-13 14:32:27 -0400

    Show custom error message

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 // Global Variables
 
-const button = document.getElementsByClassName("form-button-main");
-const input = document.getElementsByClassName("form-email");
-const errorIcon = document.getElementsByClassName("form-error-icon");
-const errorMessage = document.getElementsByClassName("error-message");
+const button = document.querySelector(".form-button-main");
+const input = document.querySelector(".form-email");
+const errorIcon = document.querySelector(".form-error-icon");
+const errorMessage = document.querySelector(".error-message");
+console.log(errorIcon)
 
-console.log(button);
-console.log('hello')
+// show error message
+button.addEventListener("click", function() {
+    if (input.validity.typeMismatch) {
+        
+        errorMessage.style.visibility = "visible";
+        errorIcon.style.visibility = "visible";
+        input.setCustomValidity(' ');
+    }
+})
